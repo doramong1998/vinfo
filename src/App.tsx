d23a04d5b3007dabaf38cf462bb961a76f334226
huytq <trangquanghuy1998@gmail.com>
2024-10-28 23:50:18 +0700

    update: ver spicer

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,21 @@
 import Vietnam from './components/Vietnam';
 import { useMemo, useState } from 'react';
 import dataJson from './assets/data.json';
+import { initializeApp } from 'firebase/app';
+import { getAnalytics } from 'firebase/analytics';
 
+const firebaseConfig = {
+  apiKey: 'AIzaSyCjfg-xog8VNHZjppPJY1_puhgCOTEr7eA',
+  authDomain: 'usopp-5ee60.firebaseapp.com',
+  projectId: 'usopp-5ee60',
+  storageBucket: 'usopp-5ee60.appspot.com',
+  messagingSenderId: '847289515947',
+  appId: '1:847289515947:web:35447508b968d4f46bdcee',
+  measurementId: 'G-5RF8MPD40F',
+};
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 function App() {
   const [province, setProvince] = useState<any>(null);
 
@@ -11,7 +25,6 @@ function App() {
     }
   }, [province]);
 
-  console.log(dataProvince);
   return (
     <div className='w-full'>
       <div className='m-auto min-h-screen pb-12  xl:p-0 p-4'>
@@ -34,3 +47,13 @@ function App() {
 }
 
 export default App;
+
+// Import the functions you need from the SDKs you need
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
+// Initialize Firebase
